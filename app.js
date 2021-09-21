e1be4eec0f0e519a157ccc96c6511bc0104110ee
Gabriel Sousa <jgss.gabriel.sousa@gmail.com>
2021-09-21 19:25:11 -0300

    Save conversions

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const decValueElement = document.getElementById("decimal-input");
 const binValueElement = document.getElementById("binary-input");
 const helpText = document.getElementById("help-text");
+const interface = document.getElementById("interface");
 
 lastChanged = "none";
-
+calculated = false;
 
 function removeLetters(){
     decValueElement.value = decValueElement.value.replace(/[^0-9]/g, '');
@@ -41,6 +42,8 @@ function convert(){
     if(!checkValues())
         return;
 
+    calculated = true;
+
     if(lastChanged == "decimal"){
         binValueElement.value = parseInt(decValueElement.value, 10).toString(2);
     }
@@ -52,4 +55,36 @@ function convert(){
 
 function changed(elementType){
     lastChanged = elementType;
+    calculated = false;
+}
+
+
+function save(){
+    if(!calculated){
+        helpText.innerHTML = "Convert the values";
+        return;
+    }
+
+    if(binValueElement.value.length > 53){
+        helpText.innerHTML = "Too large value";
+        return;
+    }
+
+    calculated = false;
+
+    const table = document.getElementById("saved-values-table");
+
+    if(table.style.display == ""){
+        table.style.display = "block";
+        interface.style.justifyContent = "space-evenly";
+    }
+
+    const newRow = table.insertRow(-1);
+    let newCell = newRow.insertCell(0);
+    let newText = document.createTextNode(binValueElement.value);
+    newCell.appendChild(newText);
+
+    newCell = newRow.insertCell(1);
+    newText = document.createTextNode(decValueElement.value);
+    newCell.appendChild(newText);
 }
